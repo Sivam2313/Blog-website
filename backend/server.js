d02e4ea5918c6d76bfcf153d7f5dd51c8c016079
Sivam2313 <sivamdas7@gmail.com>
2022-11-11 00:21:09 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const connectDB = require('../backend/config/db');
 const userRoutes = require('./routes/userRoutes');
 const blogRoutes = require('./routes/blogRoutes');
 const commentRoutes = require('./routes/commentRoutes');
+const path = require('path');
+
 
 dotenv.config()
 const app = express()
@@ -14,9 +16,22 @@ app.use(express.json())
 app.use('/blogs',blogRoutes)
 app.use('/user',userRoutes)
 app.use('/comment',commentRoutes)
-app.get('/',(req,res)=>{
-    res.send('ok');
-})
+
+const __dirname1 = path.resolve();
+
+if(process.env.Node_Env=='production'){
+    app.use(express.static(path.join(__dirname1,'../frontend/build')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"../frontend/build/index.html"))
+    })
+}
+else{
+    
+    app.get('/',(req,res)=>{
+        res.send('ok');
+    })
+}
+
 
 
 
